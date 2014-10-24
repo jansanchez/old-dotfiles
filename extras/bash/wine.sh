033b33cc42a879781fa3c9aa3f5dd5a24ef387d1
Jan Sanchez <joejansanchez@gmail.com>
2014-10-23 19:11:31 -0500

    feat(dependencies) Agregando instalador para wine.

---
+++ extras/bash/wine.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+sudo apt-get install wine
+
+sudo apt-get install winetricks
+
+sudo apt-get install playonlinux
+
+sudo dpkg --add-architecture i386
+
+sudo apt-get update
+
+sudo apt-get install wine-bin:i386
+
+echo "[playonlinux instalado]"
+
